3195f4062fa3dfc2b0f1bbd73d1fbeca16629fb8
Rainymemoryy <63650705+Rainymemoryy@users.noreply.github.com>
2022-06-13 16:53:20 +0700

    config material tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+const withMT = require('@material-tailwind/react/utils/withMT')
+
+module.exports = withMT({
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       boxShadow: {
-        34: "rgba(0, 0, 0, 0.05) 0px 0px 0px 1px",
-        11: "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px",
-        42: "rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px",
-        55: "rgba(3, 102, 214, 0.3) 0px 0px 0px 3px",
-        58: "rgba(0, 0, 0, 0.15) 0px 5px 15px 0px",
-      },
+        34: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px',
+        11: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
+        42: 'rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px',
+        55: 'rgba(3, 102, 214, 0.3) 0px 0px 0px 3px',
+        58: 'rgba(0, 0, 0, 0.15) 0px 5px 15px 0px'
+      }
     },
     fontFamily: {
-      sans: ["Roboto", "sans-serif"],
-    },
+      sans: ['Roboto', 'sans-serif']
+    }
   },
-  plugins: [],
-};
+  plugins: []
+})
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        34: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px',
+        11: 'rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px',
+        42: 'rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px',
+        55: 'rgba(3, 102, 214, 0.3) 0px 0px 0px 3px',
+        58: 'rgba(0, 0, 0, 0.15) 0px 5px 15px 0px'
+      }
+    },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif']
+    }
+  },
+  plugins: []
+}
