8366aa9dccfba075be68f292d8c46dbf7b89506d
Rainymemoryy <63650705+Rainymemoryy@users.noreply.github.com>
2022-06-16 23:48:02 +0700

    fix color

--- src/component/CreateTemplate/TypeItem/Vector/VectorPreview.tsx
+++ src/component/CreateTemplate/TypeItem/Vector/VectorPreview.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useFieldArray, useFormContext } from 'react-hook-form'
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+// import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
 import { Checkbox } from '@mui/material'
 
 export default function VectorPreview({ regName }: any) {
@@ -19,30 +19,32 @@ export default function VectorPreview({ regName }: any) {
   console.log(cols)
 
   return (
-    <table className='w-full'>
+    <table className='w-full font-medium'>
       <thead>
         <tr>
           <th></th>
           {cols?.map((col: any) => (
             <>
-              <th className='font-normal'>{col.value}</th>
+              <th className='font-medium'>
+                <div>{col.value}</div>
+              </th>
             </>
           ))}
         </tr>
       </thead>
       <tbody>
-        {rows?.map((row: any) => (
-          <tr>
-            <td>{row.value}</td>
-            {cols?.map((col: any) => (
-              <>
-                <th>
-                  {/* <div className='flex h-[42px] w-full items-center justify-center opacity-50'>
-                    <CheckBoxOutlineBlankIcon />
-                  </div> */}
+        {rows?.map((row: any, index_row) => (
+          <tr className='group rounded-2xl'>
+            <td className='rounded-l px-3 group-odd:bg-slate-50'>
+              {<div>{row.value}</div>}
+            </td>
+            {cols?.map((col: any, index_col) => (
+              <td className=' group-odd:bg-slate-50 group-odd:last:rounded-r'>
+                <div className='flex h-[42px] w-full items-center justify-center'>
                   <Checkbox />
-                </th>
-              </>
+                  {/* {`${regName}.listCheckbox[${index_row}][${index_col}]`} */}
+                </div>
+              </td>
             ))}
           </tr>
         ))}
