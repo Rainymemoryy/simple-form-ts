5c7bb8c0adf1a787e72c393e16922d936182d797
Rainymemoryy <63650705+Rainymemoryy@users.noreply.github.com>
2022-06-21 10:23:26 +0700

    10h30

--- src/component/CreateTemplate/TypeItem/CheckBoxOrRadio/Another.tsx
+++ src/component/CreateTemplate/TypeItem/CheckBoxOrRadio/Another.tsx
@@ -1,9 +1,8 @@
-import { IconButton } from '@mui/material'
 import { useFormContext } from 'react-hook-form'
 import { registerItem } from '../../../../constants/regCreTemplate'
-import ClearIcon from '@mui/icons-material/Clear'
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator'
 import { itemType } from '../../../../constants/itemType'
+import { BsX } from 'react-icons/bs'
 interface Props {
   fieldArray?: any
   regName?: any
@@ -33,23 +32,20 @@ export default function Another({ fieldArray, regName, type }: Props) {
           )}
 
           <input
-            className='input-text h-8 flex-1 truncate border-dashed bg-transparent'
+            className='input-text h-8 flex-1 border-dashed group-hover:border-violet-400'
             placeholder='Khác...'
             disabled
           />
 
           <div className='flex gap-1'>
-            <IconButton
-              className='hover:text-violet-400'
+            <button
+              className='opacity-50 outline-none transition-transform hover:scale-150 hover:opacity-100'
               onClick={() => {
                 methods.setValue(`${regName}.${registerItem.isAnother}`, false)
               }}
             >
-              <ClearIcon
-                fontSize='small'
-                className='opacity-0 group-hover:opacity-100'
-              />
-            </IconButton>
+              <BsX />
+            </button>
           </div>
         </div>
       )}
