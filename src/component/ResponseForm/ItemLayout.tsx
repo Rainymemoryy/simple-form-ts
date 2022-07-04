99dc83a335f03e7f70f4f9288f9909de9893aaf4
Rainymemoryy <63650705+Rainymemoryy@users.noreply.github.com>
2022-07-04 10:22:12 +0700

    fix color

--- src/component/ResponseForm/ItemLayout.tsx
+++ src/component/ResponseForm/ItemLayout.tsx
@@ -6,22 +6,27 @@ import { TextareaAutosize } from '@mui/material'
 import { Controller, useFormContext } from 'react-hook-form'
 import { registerItem } from '../../constants/regCreTemplate'
 import Vector from './Vector'
-import DOMPurify from 'dompurify'
+import { useState } from 'react'
 
 interface Props {
   regName: string
   index: number
   fields: any
 }
-
+const randomColor = () => {
+  const color = Math.random() * 3
+  return color > 2 ? 'bg-yellow-50' : color > 1 ? 'bg-red-50' : ''
+}
 export default function ItemLayout({ regName, fields }: Props) {
   const { itemName, itemDecs, isMultiLine, itemType: type } = fields
   const { register, control } = useFormContext()
 
+  const [color] = useState(randomColor)
+
   return (
     <main className='relative flex flex-1 items-center'>
       <section
-        className={`flex flex-1 flex-col rounded bg-white p-6 outline-0`}
+        className={`flex flex-1 flex-col rounded bg-white p-6 outline-0 ${color}`}
       >
         {itemName && (
           <p className='min-h-[32px] flex-1 resize-none text-lg font-medium tracking-wide'>
@@ -43,13 +48,13 @@ export default function ItemLayout({ regName, fields }: Props) {
               {isMultiLine ? (
                 <TextareaAutosize
                   minRows={2}
-                  className='w-full resize-none rounded border px-3 py-1.5 outline-none outline-1 hover:border-violet-200 focus:border-violet-400'
+                  className='w-full resize-none rounded border bg-transparent px-3 py-1.5 outline-none outline-1 hover:border-violet-200 focus:border-violet-400'
                   placeholder='Nhập một đoạn văn'
                   {...register(`${regName}.${registerItem.textDefault}`)}
                 />
               ) : (
                 <input
-                  className='w-full truncate rounded border border-slate-200 px-3 py-1.5 opacity-100 outline-none outline-0 hover:border-violet-200 focus:border-violet-400'
+                  className='w-full truncate rounded border border-slate-200 bg-transparent px-3 py-1.5 opacity-100 outline-none outline-0 hover:border-violet-200 focus:border-violet-400'
                   placeholder='Nhập một câu ngắn'
                   {...register(`${regName}.${registerItem.textDefault}`)}
                 />
@@ -69,7 +74,7 @@ export default function ItemLayout({ regName, fields }: Props) {
                       control={control}
                       name={`${regName}.listCheckOrRadio[${index}].isCheck`}
                       render={({ field: { onChange, onBlur, value, ref } }) => (
-                        <div className='flex h-8 w-8 items-center justify-center'>
+                        <div className='flex h-10 w-10 items-center justify-center'>
                           <input
                             type='checkbox'
                             id={`${regName}.listCheckOrRadio[${index}]`}
@@ -86,7 +91,7 @@ export default function ItemLayout({ regName, fields }: Props) {
                       control={control}
                       name={`${regName}.isCheck`}
                       render={({ field: { onChange, onBlur, value, ref } }) => (
-                        <div className='flex h-8 w-8 items-center justify-center'>
+                        <div className='flex h-8 w-10 items-center justify-center'>
                           <input
                             type='radio'
                             id={`${regName}.listCheckOrRadio[${index}]`}
@@ -115,7 +120,7 @@ export default function ItemLayout({ regName, fields }: Props) {
         </div>
       </section>
 
-      <div className='absolute left-2 h-[50%] w-1 rounded-full bg-violet-400 opacity-50'></div>
+      <div className={`absolute left-2 h-[50%] w-1 rounded-full opacity-50`} />
     </main>
   )
 }
